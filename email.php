edaadf1cfa0af56fc7323aa3099ebbc61e3166bb
GeraldHnl <geraldlima2112@gmail.com>
2022-02-25 02:44:56 -0300

    teste3

---
+++ email.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+$nome = addcslashes($_POST['name']);
+$email = addcslashes($_POST['email']);
+$mensagem = addcslashes($_POST['mesage']);
+
+$to = "geraldlima2112@gmail.com";
+$subjet = "Contado - Site comafa";
+$body = "Nome: ".$nome. "\r\n".
+        "Email: ".$email. "\r\n".
+        "Mensagem: ".$mesage;
+$header = "From:comafa@industria.com"."\r\n".
+            "Repley-To:".$email."\e\n".
+            "X=Mailer:PHP/".phpversion();
+
+    if (mail($to,$subjet,$body,$header)){
+
+    echo("Email enviado com sucesso");
+}
+    else{
+    echo("O email não foi enviado");
+}
+
+}
+
+?>
